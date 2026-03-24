e7981ac502fceb99faec8fb87dc4746a6ff527ae
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-03-24 19:49:19 +0100

    feat(blink): add missing hl-groups

--- lua/pino/groups/blink_cmp.lua
+++ lua/pino/groups/blink_cmp.lua
@@ -3,17 +3,31 @@ local M = {}
 ---@param colors table<string,string>
 ---@return table<string,vim.api.keyset.highlight>
 M.get = function(colors)
-    return {
-        BlinkCmpMenuBorder = { link = "PmenuBorder" },
-        BlinkCmpKindConstant = { link = "Constant" },
-        BlinkCmpKindConstructor = { link = "Function" },
-        BlinkCmpKindFolder = { link = "Directory" },
-        BlinkCmpKindFunction = { link = "Function" },
-        BlinkCmpKindKeyword = { fg = colors.rose },
-        BlinkCmpKindProperty = { link = "Identifier" },
-        BlinkCmpKindEnum = { link = "Identifier" },
-        BlinkCmpKindField = { link = "Identifier" },
-    }
+	return {
+		BlinkCmpKindClass = { link = "Type" },
+		BlinkCmpKindColor = { link = "Special" },
+		BlinkCmpKindConstant = { link = "Constant" },
+		BlinkCmpKindConstructor = { link = "Function" },
+		BlinkCmpKindEnumMember = { link = "Constant" },
+		BlinkCmpKindEvent = { link = "Special" },
+		BlinkCmpKindFile = { fg = colors.text },
+		BlinkCmpKindFolder = { link = "Directory" },
+		BlinkCmpKindInterface = { link = "Type" },
+		BlinkCmpKindFunction = { link = "Function" },
+		BlinkCmpKindKeyword = { fg = colors.rose },
+		BlinkCmpKindMethod = { link = "Function" },
+		BlinkCmpKindModule = { link = "Directory" },
+		BlinkCmpKindEnum = { link = "Identifier" },
+		BlinkCmpKindField = { link = "Identifier" },
+		BlinkCmpKindOperator = { link = "Operator" },
+		BlinkCmpKindProperty = { link = "Identifier" },
+		BlinkCmpKindReference = { link = "Identifier" },
+		BlinkCmpKindStruct = { link = "Type" },
+		BlinkCmpKindTypeParameter = { link = "Identifier" },
+		BlinkCmpKindUnit = { link = "Constant" },
+		BlinkCmpKindValue = { link = "Constant" },
+		BlinkCmpKindVariable = { link = "Identifier" },
+	}
 end
 
 return M
